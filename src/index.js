a30ffacd5d2170fbf1e1aae2e938287034eb12d3
Christos Ntourmisis <38923288+Vaskouf@users.noreply.github.com>
2018-05-03 01:28:08 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './vLogoSpin/vLogoSpin';
+//import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
